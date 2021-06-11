827ff6a7f651c90f6e48840f81a76d365876b677
carter-willey <cjwilley23@gmail.com>
2021-06-11 13:34:28 -0500

    added main and dinosaur files. made basic dinosaur class

---
+++ dinosaur.py
@@ -0,0 +1,10 @@
+class Dinosaur:
+    def __init__(self, type, attack_power):
+        self.type = type
+        self.health = 100
+        self.energy = 0
+        self.attack_power = attack_power
+
+    def attack(self, robot):
+        pass
+
